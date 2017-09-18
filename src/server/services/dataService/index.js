4d7bbc076be803093d08e81d04f2ee1aa2866d90
Serafin <leaf466@gmail.com>
2017-09-18 16:28:49 -0700

    Ignores import/named linting rule for dataService because models is being generated dynamically

--- src/server/services/dataService/index.js
+++ src/server/services/dataService/index.js
@@ -1,21 +1,21 @@
 import r from './r'
 
 import {
-  Chapter,
-  Cycle,
-  FeedbackType,
-  Index,
-  Member,
-  Phase,
-  Pool,
-  PoolMember,
-  Project,
-  Question,
-  Response,
-  Survey,
-  SurveyBlueprint,
-  Vote,
-  errors,
+  Chapter, // eslint-disable-line import/named
+  Cycle, // eslint-disable-line import/named
+  FeedbackType, // eslint-disable-line import/named
+  Index, // eslint-disable-line import/named
+  Member, // eslint-disable-line import/named
+  Phase, // eslint-disable-line import/named
+  Pool, // eslint-disable-line import/named
+  PoolMember, // eslint-disable-line import/named
+  Project, // eslint-disable-line import/named
+  Question, // eslint-disable-line import/named
+  Response, // eslint-disable-line import/named
+  Survey, // eslint-disable-line import/named
+  SurveyBlueprint, // eslint-disable-line import/named
+  Vote, // eslint-disable-line import/named
+  errors, // eslint-disable-line import/named
 } from './models'
 
 import {
